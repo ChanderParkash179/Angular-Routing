133ccade34571ef1651b5d08f15ca396db3671c3
Chander Parkash <chanderparkash179@gmail.com>
2023-08-22 03:41:07 +0500

    admin component coding

--- src/app/modules/admin/components/header/header.component.ts
+++ src/app/modules/admin/components/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void { }
 
+  constructor() { }
+
+  logout() {
+
+  }
 }
