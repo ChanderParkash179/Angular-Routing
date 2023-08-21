7c37271fae8c3c7abc76a0c7990be0eac03628f3
Chander Parkash <chanderparkash179@gmail.com>
2023-08-22 03:50:30 +0500

    removed stylesheet import lines

--- src/app/modules/admin/components/services/services.component.ts
+++ src/app/modules/admin/components/services/services.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  templateUrl: './services.component.html'
 })
 export class ServicesComponent {
 
